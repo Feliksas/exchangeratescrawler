f8826cf590f98b74f8b6297f5e63f86ce2ec5627
Andrey Ignatov <andrey.ignatov@ctco.lv>
2019-09-11 15:45:58 +0300

    gson pretty-printing

--- src/main/java/exchangerates/Main.java
+++ src/main/java/exchangerates/Main.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 import com.google.gson.Gson;
 
+import com.google.gson.GsonBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -38,7 +39,7 @@ public class Main {
         LocalDateTime today = LocalDate.now().atStartOfDay();
         HashMap<String, Map<LocalDateTime,Float>> dataFromDB = dbHandle.retrieveData(today, now);
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         LOG.info(gson.toJson(dataFromDB));
     }
 }
